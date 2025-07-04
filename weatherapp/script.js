af105434dfb409aa06babcd28cf4f0ba0dcc6539
James <schullarj@proton.me>
2025-07-04 08:38:45 -0400

    Update script.js

--- weatherapp/script.js
+++ weatherapp/script.js
@@ -1,7 +1,7 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-const apiKey = "a68835dad4fe4c85e56651af212923fd";
+const apiKey = "a1af212923fd";
 
 weatherForm.addEventListener("submit", async event => {
 
@@ -29,7 +29,7 @@ weatherForm.addEventListener("submit", async event => {
 });
 
 async function getWeatherData(city){
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${a68835dad4fe4c85e56651af212923fd}`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${a68835da923fd}`;
     const response = await fetch(apiUrl);
 
     if(!response.ok){
